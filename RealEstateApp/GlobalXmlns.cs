85cf881463b03b0b745ff68406b33121bd44d072
joelt <joel.tan.92@gmail.com>
2025-09-29 18:27:48 +0800

    initial commit

---
+++ RealEstateApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RealEstateApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RealEstateApp.Pages")]
